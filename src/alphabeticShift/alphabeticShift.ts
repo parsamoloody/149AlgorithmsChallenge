15f842ef09ec2d87903eed9d79f36791e944c5bc
parsamoloody <parsaprogramerjs@gmail.com>
2026-06-02 23:20:23 +0330

    I solved alphabeticShift

--- src/alphabeticShift/alphabeticShift.ts
+++ src/alphabeticShift/alphabeticShift.ts
@@ -1,5 +1,11 @@
 export function alphabeticShift(inputString: string): string {
-
+    let str = '';
+    for (let i = 0; i < inputString.length; i++) {
+        const cr = inputString.charCodeAt(i) + 1
+        str += String.fromCharCode(cr)
+    }
+    return str
 }
 
 console.log(alphabeticShift('crazy'));
+// I think it's will be = dsb{z
