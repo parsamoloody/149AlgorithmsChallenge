d6c0f9c0422ad39b4b8a1851ca85e55c9d60d45a
parsamoloody <parsaprogramerjs@gmail.com>
2026-06-02 20:47:30 +0330

    Solved almostIncreasingSequence

--- src/almostIncreasingSequence/almostIncreasingSequence.ts
+++ src/almostIncreasingSequence/almostIncreasingSequence.ts
@@ -1,5 +1,17 @@
 export function almostIncreasingSequence(sequence: number[]): boolean {
+    let notSequence = 0;
+    for (let i = 1; i < sequence.length; i++) {
+        if (sequence[i] <= sequence[i-1]) {
+            notSequence++
+            if (notSequence > 1) return false;
+
+            if (i > 1 && i < sequence.length - 1
+                && sequence[i] <= sequence[i-2] && sequence[i-1] 
+            ) {return false}
+        }
+    }
+    return true;
 }
 
-console.log(almostIncreasingSequence([1, 3, 2, 1])); 
-console.log(almostIncreasingSequence([1, 3,5, 2]));    
+almostIncreasingSequence([1, 3, 2, 1]);
+almostIncreasingSequence([1, 3, 2]);    
