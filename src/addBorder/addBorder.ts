ecd0d440bf51dacd75af6dc1d2831e3bf02b2bc2
parsamoloody <parsaprogramerjs@gmail.com>
2026-06-04 00:33:22 +0330

    Solved: add boarder

--- src/addBorder/addBorder.ts
+++ src/addBorder/addBorder.ts
@@ -1,5 +1,15 @@
 export function addBorder(picture: string[]): string[] {
-
+    let asterisks = '**';
+    for(let i=0;i<picture[0].length;i++){
+        asterisks.concat('*');
+    }
+    let boarders = [];
+    for(let i=0;i<picture.length;i++){
+        boarders.push('*'+picture[i])
+    }
+    boarders.unshift(asterisks);
+    boarders.push(asterisks);
+    return boarders;
 }
 
 console.log(addBorder(["abc", "ded"]))
