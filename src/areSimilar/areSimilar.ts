f2ce6ccc665e8cf5191f11c33606739dc376dbc7
moloodi <parsaprogramerjs@gmail.com>
2026-06-07 11:44:33 +0330

    Solved: areSimilar

--- src/areSimilar/areSimilar.ts
+++ src/areSimilar/areSimilar.ts
@@ -1,23 +1,20 @@
 export function areSimilar(a: number[], b: number[]): boolean {
-    const df = [];
-
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] !== b[i]) {
-            df.push(i);
-        }
-    }
-
-    if (df.length === 0) return true;
-
-    if (df.length === 2) {
-        const [i, j] = df;
-        return a[i] === b[j] && a[j] === b[i];
-    }
-
-    return false;
+  let df:number[] = [];
+  for(let i=0;i<a.length; i++){
+     if(a[i] !== b[i]){
+       df.push(i)
+     };
+  };
+  if(df.length == 0) return true
+  if(df && df.length == 2){
+    let [x, z] = df;
+    return a[x] == b[z] && a[z] == b[x]
+  }
+  return false;
+    
 }
 
 // Example usage
-console.log(areSimilar([1, 2, 3], [1, 2, 3]));
-console.log(areSimilar([1, 2, 3], [2, 1, 3]));
-console.log(areSimilar([1, 2, 2], [2, 1, 1])); 
+console.log(areSimilar([1, 2, 3], [1, 2, 3])); // true
+console.log(areSimilar([1, 2, 3], [2, 1, 3])); // true
+console.log(areSimilar([1, 2, 2], [2, 1, 1])); // false
