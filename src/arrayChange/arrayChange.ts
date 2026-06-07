1b13b42655117509fc039df894ead3e332ca2efd
parsamoloody <parsaprogramerjs@gmail.com>
2026-06-07 20:41:39 +0330

    Solve: arrayChange with O(n) algorithm

--- src/arrayChange/arrayChange.ts
+++ src/arrayChange/arrayChange.ts
@@ -1,5 +1,14 @@
 export function arrayChange(inputArray: number[]): number {
 
+    let totalIncrease = 0;
+    for (let i = 1; i < inputArray.length; i++) {
+        if (inputArray[i] <= inputArray[i - 1]) {
+            totalIncrease += inputArray[i - 1] - inputArray[i] + totalIncrease + 1;
+        }
+    }
+    return totalIncrease;
+
+
 }
 
-// console.log(arrayChange([1, 1, 1]));
+console.log(arrayChange([1, 1, 1]));
