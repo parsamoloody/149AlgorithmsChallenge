d059e1dba33be1148f1086821e0d6b9d7bb17da9
parsamoloody <parsaprogramerjs@gmail.com>
2026-06-10 00:41:13 +0330

    Solv: arrayPreviousLess

--- src/arrayPreviousLess/arrayPreviousLess.ts
+++ src/arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,14 @@
 export function arrayPreviousLess(items: number[]): number[] {
-
+    let lastSmallestes = [-1]
+    for (let i = 1; i < items.length; i++) {
+        const smallest = Math.max(...items.slice(i - 1, i));
+        if (items[i] <= smallest) {
+            lastSmallestes.push(-1)
+        } else {
+            lastSmallestes.push(smallest)
+        }
+    }
+    return lastSmallestes
 }
 
-// console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
