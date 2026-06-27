fd9e5da1b5cd3fe2ff9ffc1459436d3d28ee222c
parsamoloody <parsaprogramerjs@gmail.com>
2026-06-27 21:09:46 +0330

    Solve: i solved the arrayConversion

--- src/arrayConversion/arrayConversion.ts
+++ src/arrayConversion/arrayConversion.ts
@@ -1,5 +1,23 @@
 export function arrayConversion(inputArray: number[]): number {
+  let iteration = 1;
+  let arr = inputArray;
 
+  while (arr.length > 1) {
+    const newArr: number[] = [];
+
+    for (let i = 0; i < arr.length; i += 2) {
+      if (iteration % 2 === 1) {
+        newArr.push(arr[i] + arr[i + 1]);
+      } else {
+        newArr.push(arr[i] * arr[i + 1]);
+      }
+    }
+
+    arr = newArr;
+    iteration++;
+  }
+
+  return arr[0];
 }
 
-// console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]));
