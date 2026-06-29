34822a00d28b686939c8ca14305e461f5fbe28ea
parsamoloody <parsaprogramerjs@gmail.com>
2026-06-29 23:54:04 +0330

    solved same thing one line conditions

--- src/arrayPreviousLess/arrayPreviousLess.ts
+++ src/arrayPreviousLess/arrayPreviousLess.ts
@@ -3,10 +3,8 @@ export function arrayPreviousLess(items: number[]): number[] {
 
     for (let i = 1; i < items.length; i++) {
         const smallest = Math.max(...items.slice(i - 1, i));
-        if (smallest > items[i]) { result.push(-1) }
-        else { result.push(smallest) }
+        (smallest > items[i]) ? result.push(-1) : result.push(smallest)
     }
-
     return result;
 }
 
