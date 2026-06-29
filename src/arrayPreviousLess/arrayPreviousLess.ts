76b97c335d450d54d0ba0358ff30203533cf6437
parsamoloody <parsaprogramerjs@gmail.com>
2026-06-29 23:57:22 +0330

    doc: added  guid and result comment

--- src/arrayPreviousLess/arrayPreviousLess.ts
+++ src/arrayPreviousLess/arrayPreviousLess.ts
@@ -1,11 +1,12 @@
 export function arrayPreviousLess(items: number[]): number[] {
     let result = [-1]
-
     for (let i = 1; i < items.length; i++) {
+        // Check for smallest item start from the left side(reverce)
         const smallest = Math.max(...items.slice(i - 1, i));
+        // check for if smallest to push the value, otherwise, push -1
         (smallest > items[i]) ? result.push(-1) : result.push(smallest)
     }
     return result;
 }
 
-console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
+console.log(arrayPreviousLess([3, 5, 2, 4, 5])); // [-1, 3, -1, 2, 4]
