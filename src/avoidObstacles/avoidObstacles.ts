d4ba114f1f65bc94fdfb17eaa0c7eadc14a9209e
parsamoloody <parsaprogramerjs@gmail.com>
2026-07-01 22:36:56 +0330

    solved: avoidObstacles

--- src/avoidObstacles/avoidObstacles.ts
+++ src/avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,18 @@
 export function avoidObstacles(inputArray: number[]): number {
+    const sorted = inputArray.sort((a, b) => a - b);
+    const mxcle = sorted[sorted.length - 1];
 
+    for (let jump = 2; jump <= mxcle + 1; jump++) {
+        let works = true;
+        for (let pos = jump; pos <= mxcle; pos += jump) {
+            if (sorted.includes(pos)) {
+                works = false;
+                break;
+            }
+        }
+        if (works) {
+            return jump;
+        }
+    }
+    return mxcle + 1;
 }
-
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
